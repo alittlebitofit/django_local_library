faf0dba2021499ee63660bc971b8cebe2122974a
alittlebitofit <emyanon@pm.me>
2024-06-23 09:53:23 +0530

    Tutorial part 4 done: Admin site

--- catalog/models.py
+++ catalog/models.py
@@ -44,9 +44,9 @@ class Book(models.Model):
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13,
-                            unique=True,
-                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
-                                      '">ISBN number</a>')
+        unique=True,
+        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+            '">ISBN number</a>')
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
@@ -64,8 +64,8 @@ class Book(models.Model):
         return reverse('book-detail', args=[str(self.id)])
 
     def display_genre(self):
-	    """Create a string for the Genre. This is required to display genre in Admin."""
-	    return ', '.join(genre.name for genre in self.genre.all()[:3])
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
 
     display_genre.short_description = 'Genre'
 
